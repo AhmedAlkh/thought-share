cfdb327047e6eb6e2720d383f3e71750405728e1
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-02-19 13:17:13 -0500

    deleted reaction model, updated thought model, added to model index

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,64 @@
-// thoughts in User.js file will reference this file
-// require dateFormat file when I create it
-// require Reaction.js file?
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-/*
-thoughtText
-- String
-- Required
-- Must be between 1 and 280 chars
-*/
-/*
-createdAt
-- Date
-- Set default
-- Use a getter method to format the timestamp on query (this will need to be required)
-*/
-/*
-username (user that created this thought)
-- String
-- Required
-*/
-/*
-reactions(these are like replies)
-- Array of nested documents created with the reactionSchema
-*/
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,   // Customize this
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    userName: {   //user that created this thought
+        type: String,
+        required: true
+    },
+    reactions: []     /*reactions(these are like replies)
+                       - Array of nested documents created with the reactionSchema */
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+    const ReactionSchema = new Schema({
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+       reactionBody: {
+           type: String,
+           required: true,    // Customize this
+           maxlength: 280
+       },
+       userName: {
+           type: String,
+           required: true
+       },
+       createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+       }
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
 
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on the query.
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
-// module.exports = Thought; ?
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
