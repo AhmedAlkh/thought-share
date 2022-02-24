594c08c16f3b32ccd98636e95989d58e98dfa6f7
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-02-24 15:28:56 -0500

    updated main index for routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-// all routes will be here
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>404 Error!</h1>');
+});
+
+module.exports = router;
