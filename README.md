55a5ad1385ac54af38e077fab256d77cc3696e9f
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-02-24 17:07:52 -0500

    add friends is functional

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 finishing touches:
 Update required feilds to have custom messages
 Format everything with prettier?
+
+FIX!
+Deleting thoughts
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -77,21 +77,34 @@ const userController = {
 
    // POST add friend - /api/users/:userId/friends/:friendId
    // POST to add a new friend to a user's friend list
-   addFriend({ params }, res) {
-       User.findOneAndUpdate(
-           { _id: params.userId },
-           { $push: { friends: params.friendsId } },
-           { new: true }
-       )
-       .then(dbUserData => {
-           if (!dbUserData) {
-               res.status(404).json({ message: 'User not found' });
-               return;
-           }
-           res.json(dbUserData);
-       })
-       .catch(err => res.status(400).json(err));
-   },
+addFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $addToSet: { friends: params.friendId } },
+        { new: true }
+    )
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+        User.findOneAndUpdate(
+            { _id: params.friendId },
+            { $addToSet: { friends: params.userId } },
+            { new: true }
+        )
+        .then(dbFriendData => {
+            if(!dbFriendData) {
+                res.status(404).json({ message: 'User not found' })
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    })
+    .catch(err => res.status(400).json(err));
+},
+
    // DELETE to remove a friend from a user's friend list
 
 deleteFriend({ params }, res) {
