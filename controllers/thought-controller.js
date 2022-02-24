8fb4ea7cb37191f939d5e79a9831f48c76874753
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-02-24 14:56:08 -0500

    updated thought get, post, put, delete

---
+++ controllers/thought-controller.js
@@ -0,0 +1,111 @@
+const { User, Thought} = require('../models');
+
+const thoughtController = { // /api/thoughts
+// GET all thoughts
+GetAllThoughts(req, res) {
+    Thought.find({})
+    .populate({
+        path: 'reactions',
+        select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+},
+// GET single thought by its _id
+getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+    .populate({
+        path: 'reactions',
+        select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
+            .then(dbThoughtData => {
+                // If no thought is found, send 404
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'Thought not found' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+},
+/*
+ POST to create a new thought
+ (don't forget to push the created thought's _id to the associated user's thoughts array field)
+ example data
+{
+    "thoughtText": "Here's a cool thought...",
+    "username": "lernantino",
+    "userId": "5edff358a0fcb779aa7b118b"
+  }
+*/
+createThought({ body }, res) {
+    Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+},
+// PUT to update a thought by its _id
+updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+        if (!dbThoughtData) {
+        res.status(404).json({ message: 'Thought not found' });
+        return;
+        }
+        res.json(dbThoughtData);
+     })
+    .catch(err => res.status(400).json(err));
+},
+// DELETE to remove a thought by its _id
+deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+       .then(dbThoughtData => {
+           if (!dbThoughtData) {
+               res.status(404).json({ message: 'Thought not found' });
+               return;
+           }
+           return User.findOneAndUpdate(
+               { _id: params.userId },
+               { $pull: { thoughts: params.id } },
+               { new: true}
+           )
+       })
+       .then(dbUserData => {
+           if (!dbUserData) {
+               res.status(404).json({ message: 'User not found' });
+               return;
+           }
+           res.json(dbUserData);
+       })
+       .catch(err => res.status(400).json(err));
+},
+// ==================
+// REACTION ROUTES
+// /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in a single thought's reactions array field
+
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+}
